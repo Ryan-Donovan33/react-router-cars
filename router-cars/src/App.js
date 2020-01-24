f3bf4ef7362fe73e7d6111caa29e23c0bafd538d
Ryan-Donovan33 <55716909+Ryan-Donovan33@users.noreply.github.com>
2020-01-24 08:53:42 -0500

    Adding in Card and Car functions

--- router-cars/src/App.js
+++ router-cars/src/App.js
@@ -1,26 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import { data } from './data';
+import './App.scss';
+import { Route } from 'react-router-dom';
+import Landing from './Components/Landing/Landing';
+import Form from './Components/Form/Form';
+import Car from './Components/Car/Car';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [ cars, setCars ] = useState([]);
+
+	useEffect(() => {
+		setCars(data);
+	}, []);
+
+	//axios should be called here in useEffect to retrive the data from the api. Look at older projects for help with this. Look at possibly the last sprint challenge with star wars
+
+	return (
+		<div className="App">
+			<Route path="/" exact>
+				<Landing cars={cars} setCars={setCars} />
+			</Route>
+
+			<Route path="/add">
+				<Form cars={cars} setCars={setCars} />
+			</Route>
+
+			<Route path="/cars/:id">
+				<Car cars={cars} setCars={setCars} />
+			</Route>
+		</div>
+	);
 }
 
 export default App;
