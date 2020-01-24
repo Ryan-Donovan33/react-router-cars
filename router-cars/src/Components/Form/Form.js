41d16834570052d9e3b5c3bda6cf639d3307a0d2
Ryan-Donovan33 <55716909+Ryan-Donovan33@users.noreply.github.com>
2020-01-24 08:56:33 -0500

    Added in Form and Landing Functions

---
+++ router-cars/src/Components/Form/Form.js
@@ -0,0 +1,33 @@
+// react hook form -- may want to use Formik like the User Onboarding project but if we are able to use react hook then follow this example.
+
+import React from 'react';
+import { useForm } from 'react-hook-form';
+import './Form.scss';
+import { useHistory } from 'react-router-dom';
+
+function Form({ setCars, cars }) {
+	const { register, handleSubmit, watch, errors } = useForm();
+	const history = useHistory();
+
+	const onSubmit = (data) => {
+		const carToBeAdded = { ...data, id: Math.random() };
+		setCars([ ...cars, carToBeAdded ]);
+		history.push('/');
+	};
+	return (
+		<div className="form-wrapper">
+			<h1>Add A Car</h1>
+			<form onSubmit={handleSubmit(onSubmit)}>
+				<input placeholder="Make" name="make" ref={register({ required: true })} />
+				{errors.make && <span>Make is required</span>}
+				<input placeholder="Model" name="model" ref={register({ required: true })} />
+				{errors.model && <span>Model is required</span>}
+				<input placeholder="Price" name="price" ref={register({ required: true, pattern: [ 0 - 9 ] })} />
+				{errors.price && <span>Price Must Be a Number</span>}
+				<input type="submit" />
+			</form>
+		</div>
+	);
+}
+
+export default Form;
